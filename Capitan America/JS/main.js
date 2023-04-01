1fe896df4fd592883d1c8a3eb31d2fb0435336e1
Elvis182021 <103191472+Elvis182021@users.noreply.github.com>
2023-04-01 11:21:13 -0500

    Showdate
    Mostrar hora actual

---
+++ Capitan America/JS/main.js
@@ -0,0 +1,11 @@
+//funcion 
+function showdate(){
+    document.getElementById("datet");
+    document.getElementById("dateShow").innerHTML= Date();
+}
+
+
+function Showdate(){
+    document.getElementById("dateT");
+    document.getElementById("dateShoww").innerHTML= Date();
+}
