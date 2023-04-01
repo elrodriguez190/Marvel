fcf47f66f68ccec28498be1d207ec9992db1f85a
andresfuentescristiano <andresfuentescristiano@gmail.com>
2023-04-01 11:34:44 -0500

    add function Date
    add function Date

---
+++ ironman/JS/Main.js
@@ -0,0 +1,6 @@
+// {}
+function Showdate(){
+    document.getElementById('dateT');
+   let dateShow = document.getElementById('dateShow');
+   dateShow.innerHTML=(`<h1> la fecha de hoy es :</h1>`+ Date());
+}
