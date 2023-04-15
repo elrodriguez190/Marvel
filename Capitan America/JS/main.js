0570398dac752fc4dfabacd7e73d9cadd13e008c
Elvis182021 <103191472+Elvis182021@users.noreply.github.com>
2023-04-15 11:19:08 -0500

    Titulo del proyecto
    Agregamos el nombre con Javascript en el body

--- Capitan America/JS/main.js
+++ Capitan America/JS/main.js
@@ -1,11 +1,9 @@
+
+document.getElementById("nombreSuperHeroe").innerHTML = "Capitan America";
+
 //funcion 
 function showdate(){
     document.getElementById("datet");
     document.getElementById("dateShow").innerHTML= Date();
 }
 
-
-function Showdate(){
-    document.getElementById("dateT");
-    document.getElementById("dateShoww").innerHTML= Date();
-}
