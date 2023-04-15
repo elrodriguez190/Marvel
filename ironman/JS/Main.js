c276a9427af9c61f4e8b06489294de3df9cb6135
Andres Felipe Fuentes Cristiano <andresfelipefuentescritaino@gmail.com>
2023-04-15 11:21:45 -0500

    agragando  titulo Del superheroe
    agregando el nombre de  mi  superheroe

--- ironman/JS/Main.js
+++ ironman/JS/Main.js
@@ -4,3 +4,7 @@ function Showdate(){
    let dateShow = document.getElementById('dateShow');
    dateShow.innerHTML=(`<h1> la fecha de hoy es :</h1>`+ Date());
 }
+/*function nombreSuperhero(){
+   let nombreSuperhero = document.getElementById("nombreSuperhero");
+   nombreSuperhero.innerHTML=('Ironman');
+}*/
