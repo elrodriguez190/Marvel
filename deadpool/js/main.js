4ed2dba2b04fb613215586fde25caa2cb3e912ce
thomasdemichelle <106881889+thomasdemichelle@users.noreply.github.com>
2023-04-15 11:19:01 -0500

    nombre
    agrege el nombre del super heroe

--- deadpool/js/main.js
+++ deadpool/js/main.js
@@ -2,3 +2,8 @@ function Showdate(){
     document.getElementById('date')
     document.getElementById('dateshow').innerHTML=Date()
 }
+
+document.getElementById('nombre_superhero').innerHTML="deadpool"
+
+
+    
