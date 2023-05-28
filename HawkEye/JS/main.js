54b09f8bac54ac56fe4b76f2fc47f0418344ede7
YamCast <100479274+YamCast@users.noreply.github.com>
2023-05-28 17:01:25 -0500

    cambios
    cambios

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -1,25 +1,22 @@
 
-var settings = {
-    "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
-    "method": "GET",
-    "timeout:0,"
-}
 
-$.ajax(settings).done(function (response)) {
-    console.log(response);
-}
+function Showdate(){
+    document.getElementById("dateT")
+    document.getElementById("dateShow").innerHTML=Date();
+ }
+
+document.getElementById("nombreHero").innerHTML="HawkEye"
 
 
 
 
+// var settings = {
+//     "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
+//     "method": "GET",
+//     "timeout:0"}
 
 
-
-
-
-// function Showdate(){
-//     document.getElementById("dateT")
-//     document.getElementById("dateShow").innerHTML=Date();
+// $.ajax(settings).done (function (response)){
+//     console.log(response);
 // }
 
-// document.getElementById("nombreHero").innerHTML="HawkEye"
