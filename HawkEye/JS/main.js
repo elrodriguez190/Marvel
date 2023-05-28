e7662ab3a47d484360cb4e3b802f593ab3851fa4
YamCast <100479274+YamCast@users.noreply.github.com>
2023-05-27 22:53:51 -0500

    hoy
    today

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -1,6 +1,25 @@
-function Showdate(){
-    document.getElementById("dateT")
-    document.getElementById("dateShow").innerHTML=Date();
+
+var settings = {
+    "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
+    "method": "GET",
+    "timeout:0,"
 }
 
-document.getElementById("nombreHero").innerHTML="HawkEye"
+$.ajax(settings).done(function (response)) {
+    console.log(response);
+}
+
+
+
+
+
+
+
+
+
+// function Showdate(){
+//     document.getElementById("dateT")
+//     document.getElementById("dateShow").innerHTML=Date();
+// }
+
+// document.getElementById("nombreHero").innerHTML="HawkEye"
