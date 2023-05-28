53113ae0c1ca82b7e0de0bbccc87c21850b567eb
YamCast <100479274+YamCast@users.noreply.github.com>
2023-05-28 17:35:31 -0500

    mas
    mas

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -8,7 +8,23 @@ function Showdate(){
 document.getElementById("nombreHero").innerHTML="HawkEye"
 
 
+const marvel ={
+    render:() => {
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1';
+        const container = document.querySelector('#marvel-row');
+        let contentHTML ='';
 
+        fetch(urlAPI)
+        .then(res =>res.json())
+        .then((json) => {
+            for(const hero of json.data.results){
+                let
+            }
+        })
+    }
+};
+
+marvel.render();
 
 // var settings = {
 //     "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
