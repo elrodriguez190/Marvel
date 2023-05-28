2be14ff6855c59887ae3ab38bdf6ecba9f0d7c1d
Yeisongalindo <100476670+Yeisongalindo@users.noreply.github.com>
2023-05-27 23:05:25 -0500

    pagina
    se agrego la imagen de fondo y biografia , frente a la key de marvel no se me dio la facilidad de la conexion frente la web

--- The Punisher/JS/main.js
+++ The Punisher/JS/main.js
@@ -2,3 +2,16 @@ function showdate(){
     document.getElementById("dateshow").innerHTML= Date();
 }
 document.getElementById("The Punisher").innerHTML="The punisher"
+
+function mostrarBiografia() {
+  var biografia = document.getElementById("biografia");
+  if (biografia.style.display === "none") {
+    biografia.style.display = "block";
+  } else {
+    biografia.style.display = "none";
+  }
+}
+window.addEventListener('DOMContentLoaded', function() {
+    var biografia = document.getElementById("biografia");
+    biografia.style.display = "none";
+  })
