4af36d03d7ac9902cbfc39aa711bb3c20da9e252
YamCast <100479274+YamCast@users.noreply.github.com>
2023-06-02 23:57:32 -0500

    summary
    summary

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -1,16 +1,6 @@
-
-
-function Showdate(){
-    document.getElementById("dateT")
-    document.getElementById("dateShow").innerHTML=Date();
- }
-
-document.getElementById("nombreHero").innerHTML="HawkEye"
-
-
 const marvel ={
     render:() => {
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye';
         const container = document.querySelector('#marvel-row');
         let contentHTML ='';
 
@@ -25,14 +15,26 @@ const marvel ={
                 <a href="${urlHero}" target="_blank">
                     <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
                 </a>
+                <h3 class="title">${hero.name}</h3>
                 </div>       `;
                 
             }
+            container.innerHTML= contentHTML;
         })
     }
 };
 
 marvel.render();
+
+
+
+function Showdate(){
+    document.getElementById("dateT")
+    document.getElementById("dateShow").innerHTML=Date();
+ }
+
+document.getElementById("nombreHero").innerHTML="HawkEye"
+
 
 // var settings = {
 //     "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
