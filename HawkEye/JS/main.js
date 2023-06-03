990092727f5e5434cc17c75d5dd3e4d575b1f4bc
YamCast <100479274+YamCast@users.noreply.github.com>
2023-06-03 18:23:40 -0500

    por hoy
    poy

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -1,39 +1,61 @@
-const marvel ={
-    render:() => {
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye';
-        const container = document.querySelector('#marvel-row');
-        let contentHTML ='';
+var settings={
+    "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye",
+    "method": "GET",
+    "timeout" :0,
+}
 
-        fetch(urlAPI)
-        .then(res =>res.json())
-        .then((json) => {
-            for(const hero of json.data.results){
-                let urlHero = hero.urls[0].url;
-                
-                contentHTML == `              
-               <div class="col-md-4">
-                <a href="${urlHero}" target="_blank">
-                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-                </a>
-                <h3 class="title">${hero.name}</h3>
-                </div>       `;
-                
-            }
-            container.innerHTML= contentHTML;
-        })
-    }
-};
 
-marvel.render();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 function Showdate(){
     document.getElementById("dateT")
     document.getElementById("dateShow").innerHTML=Date();
- }
+}
 
 document.getElementById("nombreHero").innerHTML="HawkEye"
+// const marvel ={
+//     render:() => {
+//         const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye';
+//         const container = document.querySelector('#marvel-row');
+//         let contentHTML ='';
+
+//         fetch(urlAPI)
+//         .then(res =>res.json())
+//         .then((json) => {
+//             for(const hero of json.data.results){
+//                 let urlHero = hero.urls[0].url;
+                
+//                 contentHTML == `              
+//                <div class="col-md-4">
+//                 <a href="${urlHero}" target="_blank">
+//                     <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+//                 </a>
+//                 <h3 class="title">${hero.name}</h3>
+//                 </div>       `;
+                
+//             }
+//             container.innerHTML= contentHTML;
+//         })
+//     }
+// };
+
+// marvel.render();
+
+//fecha
+
+
 
 
 // var settings = {
