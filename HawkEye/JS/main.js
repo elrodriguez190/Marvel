230435a8e4e3197233720587c6ec3f4405d5d106
YamCast <100479274+YamCast@users.noreply.github.com>
2023-06-15 20:56:21 -0500

    yes
    yes

--- HawkEye/JS/main.js
+++ HawkEye/JS/main.js
@@ -2,21 +2,23 @@ var settings={
     "url": "https://gateway.marvel.com:443/v1/public/characters/1009338?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&limit=100",
     "method": "GET",
     "timeout" :0,
-}
-
-$.ajax(settings) .done(function (response) {
-    console.log(response);
-    var information =response.data.results[0];
-    console.log(information);
-    console.log(information.thumbnail.path+","+information.thumbnail.extension)
+};
 
     //Guardando datos 
     //llave publica 4b525502df661367697f4ddc61942a24
     //llave privada aac8b70446ec89d9989e3ee9f78b6b3b489f81ba
 
-    // https://gateway.marvel.com:443/v1/public/characters/1009228?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&limit=100
-    //https://gateway.marvel.com:443/v1/public/characters/1009228?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye&id=1009338
+    // https://gateway.marvel.com:443/v1/public/characters/1009228?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&limit=100  --anterior
+    //https://gateway.marvel.com:443/v1/public/characters/1009228?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye&id=1009338 --otra anterior
 
+
+$.ajax(settings) .done(function (response) {
+    console.log(response);
+    var information =response.data.results[0];
+    console.log(information);
+    console.log(information.thumbnail.path+"."+information.thumbnail.extension)
+
+ 
     class SuperHeroe {
         constructor(name, img){
             this.name = name;
@@ -29,9 +31,9 @@ $.ajax(settings) .done(function (response) {
     miSuperheroe.img = information.thumbnail.path+"."+information.thumbnail.extension;
     console.log(miSuperheroe.name);
 
-    var nombres = document.getElementsById('nombreSuperHeroe');
-    ImageBitmapRenderingContext.src = miSuperheroe.name;
-
+    var nombres = document.getElementsByClassName('nombreSuperHeroe');
+    nombres.innerHTML = miSuperheroe.name;
+    // ImageBitmapRenderingContext.src = miSuperheroe.name;
 
     var imagen = document.getElementById("imgSuperHeroe");
     imagen.src = miSuperheroe.img;
@@ -40,7 +42,7 @@ $.ajax(settings) .done(function (response) {
 }) ;
 
 var character = "prueba";
-console.log(imagen);
+console.log(character);
 
 //boton fecha
 function Showdate(){
@@ -51,49 +53,19 @@ function Showdate(){
 document.getElementById("nombreHero").innerHTML="HawkEye"
 
 
-//VIEJO VIEJO VIEJO
+//biografia
+function toggle(obj) {
+    var obj=document.getElementById(obj);
+    if (obj.style.display == "block") obj.style.display = "none";
+    else obj.style.display = "block";
+}
 
-// const marvel ={
-//     render:() => {
-//         const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1&name=hawkeye';
-//         const container = document.querySelector('#marvel-row');
-//         let contentHTML ='';
-
-//         fetch(urlAPI)
-//         .then(res =>res.json())
-//         .then((json) => {
-//             for(const hero of json.data.results){
-//                 let urlHero = hero.urls[0].url;
-                
-//                 contentHTML == `              
-//                <div class="col-md-4">
-//                 <a href="${urlHero}" target="_blank">
-//                     <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-//                 </a>
-//                 <h3 class="title">${hero.name}</h3>
-//                 </div>       `;
-                
-//             }
-//             container.innerHTML= contentHTML;
-//         })
-//     }
-// };
-
-// marvel.render();
-
-//fecha
+//peliculas
+function getMovies(){
+    var hawkeye = new SuperHeroe();
+    var pelis = miSuperheroe.name;
+    console.log(pelis);
+}
 
 
-
-
-// var settings = {
-//     "url": "https://gateway.marvel.com:443/v1/public/characters?apikey=4b525502df661367697f4ddc61942a24&hash=1f182aa8b805a294b1d4537e8292b15c&ts=1",
-//     "method": "GET",
-//     "timeout:0"}
-
-
-// $.ajax(settings).done (function (response)){
-//     console.log(response);
-// }
-//
 
